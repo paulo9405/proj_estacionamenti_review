96807bef6553305625d93510ec739775848d6b33
Paulo <paulo.ricardo1137.pr@gmail.com>
2024-11-27 15:21:37 +0000

    updating admin

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,64 @@
 from django.contrib import admin
 from core.models import *
 
-admin.site.register(Pessoa)
-admin.site.register(Parametros)
-admin.site.register(Marca)
-admin.site.register(Veiculo)
-admin.site.register(MovRotative)
-admin.site.register(Mensalista)
-admin.site.register(MovMensalista)
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome',
+        'endereco',
+        'telefone'
+    )
+
+
+class ParametroAdmin(admin.ModelAdmin):
+    list_display = (
+        'valor_hora',
+        'valor_mes'
+    )
+
+
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = (
+        'nome',
+    )
+
+
+class MensalistaAdmin(admin.ModelAdmin):
+    list_display = (
+        'veiculo',
+        'inicio',
+        'valor_mes'
+    )
+
+
+class MovMenssalistaAdmin(admin.ModelAdmin):
+    list_display = (
+        'mensalista',
+        'data_pagamento',
+        'total'
+    )
+
+
+class VeiculoAdmin(admin.ModelAdmin):
+    list_display = (
+        'marca',
+        'placa',
+        'proprietario',
+        'cor',
+        'observacoes'
+    )
+
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = (
+        'checkin', 'checkout', 'valor_hora', 'veiculo',
+        'pago')
+
+
+admin.site.register(Pessoa, PessoaAdmin)
+admin.site.register(Parametros, ParametroAdmin)
+admin.site.register(Marca, MarcaAdmin)
+admin.site.register(Veiculo, VeiculoAdmin)
+admin.site.register(MovRotative, MovRotativoAdmin)
+admin.site.register(Mensalista, MensalistaAdmin)
+admin.site.register(MovMensalista, MovMenssalistaAdmin)
--- core/models.py
+++ core/models.py
@@ -58,7 +58,7 @@ class MovRotative(models.Model):
 class Mensalista(models.Model):
     veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
     inicio = models.DateField()
-    valor_mes = models.DecimalField(max_digits=6, max_length=2)
+    valor_mes = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return str(self.veiculo) + ' - ' + str(self.inicio)
