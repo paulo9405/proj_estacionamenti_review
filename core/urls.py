3d46391daba92342ce3bc8e3dc475ebb44cda596
Paulo <paulo.ricardo1137.pr@gmail.com>
2024-11-28 17:48:06 +0000

    new person function cretated

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import home, lista_pessoas, lista_veiculos, lista_movrotativo
+from .views import (
+        home,
+        lista_pessoas,
+        lista_veiculos,
+        lista_movrotativo,
+        pessoa_novo
+)
 
 urlpatterns = [
         path('index/', home, name='core_home'),
         path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
+        path('pessoas-novo/', pessoa_novo, name='core_pessoa_novo'),
         path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
         path('mov_rotativo/', lista_movrotativo, name='core_lista_movrotativo'),
 ]
